63b70fde8d4b8afd3917514033569768e1d26ed6
Matias <moralesmatias193@gmail.com>
2020-01-06 02:06:37 -0300

    v2.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
-	$(function () {
-		$('[data-toggle="tooltip"]').tooltip()
-	})
+	// navigation click actions	
+	$('.scroll-link').on('click', function(event){
+		event.preventDefault();
+		var sectionID = $(this).attr("data-id");
+		scrollToID('#' + sectionID, 750);
+	});
 });
+
+// scroll function
+function scrollToID(id, speed){
+	var offSet = 10;
+	var targetOffset = $(id).offset().top - offSet;
+	var mainNav = $('#main-nav');
+	$('html,body').animate({scrollTop:targetOffset}, speed);
+}
